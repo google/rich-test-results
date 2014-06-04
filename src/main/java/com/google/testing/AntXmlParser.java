68c195496e51f923023ad6accb29c378f2b710e7
Peter Epstein <pepstein@google.com>
2014-06-04 13:33:35 -0700

    Add failure and error to TestCase proto, and support parsing the XML to populate them.
    Change-Id: Id2e427b486ea00f9f7e773507c74639f4fd12f1b

--- src/main/java/com/google/testing/AntXmlParser.java
+++ src/main/java/com/google/testing/AntXmlParser.java
@@ -1,22 +1,40 @@
 package com.google.testing;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.protobuf.TextFormat;
 import com.google.testing.TestSuiteProto.Property.Builder;
+import com.google.testing.TestSuiteProto.StackFrame;
+import com.google.testing.TestSuiteProto.StackTrace;
 import com.google.testing.TestSuiteProto.TestCase;
 import com.google.testing.TestSuiteProto.TestSuite;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.nio.charset.Charset;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import java.io.InputStream;
-import java.nio.charset.Charset;
 
 /**
  * STaX parser for the Ant (Junit task) XML test results format.
  * @author alexeagle@google.com (Alex Eagle)
  */
 public class AntXmlParser {
+
+  private static final String STACK_FRAME_PREFIX = "\tat ";
   XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+
+  public static void main(String[] args) throws IOException {
+    String path = args[0];
+    TestSuite testSuite = new AntXmlParser().parse(new FileInputStream(path), UTF_8);
+    TextFormat.print(testSuite, System.out);
+  }
 
   public TestSuite parse(InputStream in, Charset encoding) {
     TestSuite.Builder builder = TestSuite.newBuilder();
@@ -121,7 +139,8 @@ public class AntXmlParser {
     } while (!xmlStreamReader.isEndElement() || !"properties".equals(tagName));
   }
 
-  private void parseTestCase(XMLStreamReader xmlStreamReader, TestSuite.Builder suiteBuilder) {
+  private void parseTestCase(XMLStreamReader xmlStreamReader, TestSuite.Builder suiteBuilder)
+      throws XMLStreamException {
     TestCase.Builder builder = suiteBuilder.addTestCaseBuilder();
     for (int i = 0; i < xmlStreamReader.getAttributeCount(); i++) {
       String attributeValue = xmlStreamReader.getAttributeValue(i);
@@ -137,6 +156,93 @@ public class AntXmlParser {
           break;
       }
     }
+
+    String tagName = null;
+    do {
+      xmlStreamReader.next();
+      if (!xmlStreamReader.hasName()) {
+        continue;
+      }
+      tagName = xmlStreamReader.getName().toString();
+      if (xmlStreamReader.isStartElement()) {
+        switch (tagName) {
+          case "failure":
+            parseStackTrace(xmlStreamReader, builder.addFailureBuilder(), "failure");
+            break;
+          case "error":
+            parseStackTrace(xmlStreamReader, builder.getErrorBuilder(), "error");
+            break;
+        }
+      }
+    } while (!xmlStreamReader.isEndElement() || !"testcase".equals(tagName));
   }
 
+  private void parseStackTrace(XMLStreamReader xmlStreamReader,
+      StackTrace.Builder stackTraceBuilder, String elementType) throws XMLStreamException {
+    for (int i = 0; i < xmlStreamReader.getAttributeCount(); i++) {
+      String attributeValue = xmlStreamReader.getAttributeValue(i);
+      switch (xmlStreamReader.getAttributeName(i).toString()) {
+        case "message":
+          stackTraceBuilder.setExceptionMessage(attributeValue);
+          break;
+        case "type":
+          stackTraceBuilder.setExceptionType(attributeValue);
+          break;
+      }
+    }
+
+    String tagName = null;
+    //TODO(pepstein): Avoid holding entire stack trace in memory.
+    StringBuilder stringBuilder = new StringBuilder();
+    do {
+      xmlStreamReader.next();
+      if (xmlStreamReader.hasName()) {
+        tagName = xmlStreamReader.getName().toString();
+      } else if (xmlStreamReader.isCharacters()) {
+        String text = xmlStreamReader.getText();
+        stringBuilder.append(text);
+      }
+    } while (!xmlStreamReader.isEndElement() || !elementType.equals(tagName));
+    String stackTrace = stringBuilder.toString();
+
+    BufferedReader reader = new BufferedReader(new StringReader(stackTrace));
+    try {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        // Ignore first line, which seems to be redundant with attributes.
+        // Ignore any other lines that don't start with "at".
+        if (line.startsWith(STACK_FRAME_PREFIX)) {
+          int openParen = line.indexOf('(');
+          int closeParen = line.indexOf(')');
+          if (openParen < 0 || closeParen < 0) {
+            throw new XMLStreamException("Error parsing stack trace: " + line);
+          }
+          String classAndMethod = line.substring(STACK_FRAME_PREFIX.length(), openParen);
+          String fullyQualifiedClassname = classAndMethod
+              .substring(0, classAndMethod.lastIndexOf('.'));
+          String methodName = classAndMethod.substring(classAndMethod.lastIndexOf('.') + 1);
+          StackFrame.Builder stackFrameBuilder = stackTraceBuilder.addStackFrameBuilder()
+              .setFullyQualifiedClassname(fullyQualifiedClassname)
+              .setMethodName(methodName);
+
+
+          String fileAndLine = line.substring(openParen + 1, closeParen);
+          int colon = fileAndLine.indexOf(':');
+          if (colon > 0) {
+            String filename = fileAndLine.substring(0, colon);
+            int lineNumber = Integer.parseInt(fileAndLine.substring(colon + 1));
+            stackFrameBuilder
+                .setFilename(filename)
+                .setLineNumber(lineNumber);
+          } else {
+            //TODO(pepstein): Record the text in parens, which might be "Native Method".
+          }
+        }
+      }
+    } catch (IOException e) {
+      throw new XMLStreamException("Error parsing stack trace", e);
+    } catch (NumberFormatException e) {
+      throw new XMLStreamException("Error parsing line number in stack frame", e);
+    }
+  }
 }
--- src/test/java/com/google/testing/AntXmlParserTest.java
+++ src/test/java/com/google/testing/AntXmlParserTest.java
@@ -5,8 +5,11 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import com.google.testing.TestSuiteProto.Property;
+import com.google.testing.TestSuiteProto.StackFrame;
+import com.google.testing.TestSuiteProto.StackTrace;
 import com.google.testing.TestSuiteProto.TestCase;
 import com.google.testing.TestSuiteProto.TestSuite;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -16,8 +19,7 @@ import org.junit.runners.JUnit4;
  */
 @RunWith(JUnit4.class)
 public class AntXmlParserTest {
-  @Test
-  public void testShouldParseSimpleXml() throws Exception {
+  @Test public void shouldParseSimpleXml() throws Exception {
     AntXmlParser parser = new AntXmlParser();
     TestSuite actual = parser.parse(getClass().getResourceAsStream("/simple.xml"), UTF_8);
     TestSuite expected = TestSuite.newBuilder()
@@ -38,8 +40,115 @@ public class AntXmlParserTest {
         .build();
     assertThat(actual, is(expected));
   }
-  @Test
-  public void testShouldParseSimpleXmlWithComments() throws Exception {
+
+  @Test public void shouldParseTestFailure() throws Exception {
+    AntXmlParser parser = new AntXmlParser();
+    TestSuite actual = parser.parse(getClass().getResourceAsStream("/fail.xml"), UTF_8);
+    TestSuite expected = TestSuite.newBuilder()
+        .setName("com.google.SimpleTest")
+        .setTotalCount(2)
+        .setFailureCount(1)
+        .setErrorCount(0)
+        .setSkippedCount(0)
+        .setElapsedTimeMillis(6L)
+        .addTestCase(TestCase.newBuilder()
+            .setElapsedTimeMillis(6L)
+            .setClassName("com.google.SimpleTest")
+            .setName("testThatFails")
+            .addFailure(StackTrace.newBuilder()
+                .setExceptionMessage("expected:<1> but was:<2>")
+                .setExceptionType("java.lang.AssertionError")
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("org.junit.Assert")
+                    .setMethodName("fail")
+                    .setFilename("Assert.java")
+                    .setLineNumber(88))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("org.junit.Assert")
+                    .setMethodName("failNotEquals")
+                    .setFilename("Assert.java")
+                    .setLineNumber(743))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("org.junit.Assert")
+                    .setMethodName("assertEquals")
+                    .setFilename("Assert.java")
+                    .setLineNumber(118))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("org.junit.Assert")
+                    .setMethodName("assertEquals")
+                    .setFilename("Assert.java")
+                    .setLineNumber(555))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("org.junit.Assert")
+                    .setMethodName("assertEquals")
+                    .setFilename("Assert.java")
+                    .setLineNumber(542))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("com.google.SimpleTest")
+                    .setMethodName("testThatFails")
+                    .setFilename("SimpleTest.java")
+                    .setLineNumber(11))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("sun.reflect.NativeMethodAccessorImpl")
+                    .setMethodName("invoke0"))))
+        .addTestCase(TestCase.newBuilder()
+            .setElapsedTimeMillis(0L)
+            .setClassName("com.google.SimpleTest")
+            .setName("testThatPasses"))
+        .build();
+    assertThat(actual, is(expected));
+  }
+
+  @Test public void shouldParseTestError() throws Exception {
+    AntXmlParser parser = new AntXmlParser();
+    TestSuite actual = parser.parse(getClass().getResourceAsStream("/error.xml"), UTF_8);
+    TestSuite expected = TestSuite.newBuilder()
+        .setName("com.google.ExceptionThrownTest")
+        .setTotalCount(1)
+        .setFailureCount(0)
+        .setErrorCount(1)
+        .setSkippedCount(0)
+        .setElapsedTimeMillis(7L)
+        .addTestCase(TestCase.newBuilder()
+            .setElapsedTimeMillis(7L)
+            .setClassName("com.google.ExceptionThrownTest")
+            .setName("testDivision")
+            .setError(StackTrace.newBuilder()
+                .setExceptionMessage("/ by zero")
+                .setExceptionType("java.lang.ArithmeticException")
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("com.google.ExceptionThrownTest")
+                    .setMethodName("testDivision")
+                    .setFilename("ExceptionThrownTest.java")
+                    .setLineNumber(11))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("sun.reflect.NativeMethodAccessorImpl")
+                    .setMethodName("invoke0"))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("sun.reflect.NativeMethodAccessorImpl")
+                    .setMethodName("invoke")
+                    .setFilename("NativeMethodAccessorImpl.java")
+                    .setLineNumber(57))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("sun.reflect.DelegatingMethodAccessorImpl")
+                    .setMethodName("invoke")
+                    .setFilename("DelegatingMethodAccessorImpl.java")
+                    .setLineNumber(43))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("java.lang.reflect.Method")
+                    .setMethodName("invoke")
+                    .setFilename("Method.java")
+                    .setLineNumber(606))
+                .addStackFrame(StackFrame.newBuilder()
+                    .setFullyQualifiedClassname("org.junit.runners.model.FrameworkMethod$1")
+                    .setMethodName("runReflectiveCall")
+                    .setFilename("FrameworkMethod.java")
+                    .setLineNumber(47))))
+        .build();
+    assertThat(actual, is(expected));
+  }
+
+  @Test public void shouldParseSimpleXmlWithComments() throws Exception {
     AntXmlParser parser = new AntXmlParser();
     TestSuite actual = parser.parse(getClass().getResourceAsStream("/simpleWithComments.xml"), UTF_8);
     TestSuite expected = TestSuite.newBuilder()
